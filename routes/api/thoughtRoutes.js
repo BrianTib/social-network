3cf0c2e54c7d7aae43165a3c30881ff81e718307
Brian T. <bptiburcio@gmail.com>
2024-07-25 07:41:11 -0400

    update methods and routes for user

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,18 @@
 const router = require("express").Router();
+const User = require("../../models/User");
+
+/*
+
+/api/users
+
+GET all users
+
+GET a single user by its _id and populated thought and friend data
+
+POST a new user:*/
+
+router.get("/", (req, res) => {
+    User.find({});
+});
 
 module.exports = router;
